38688897cb72a3a3c19843ea7a085433e67284fb
Abdullah Bamaga <abdallahbamaga999@gmail.com>
2026-02-19 02:03:52 +0300

    Refactor subscriber validation: Extract logic to Form Request for consistency

--- app/Http/Controllers/SubscriberController.php
+++ app/Http/Controllers/SubscriberController.php
@@ -2,17 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreSubscriberRequest;
 use App\Models\Subscriber;
 use Illuminate\Http\Request;
 
 class SubscriberController extends Controller
 {
-    public function store (Request $request) {
-        $data = $request->validate([
-            'email' => 'required|email|unique:subscribers,email'
-        ] , ['email.unique' => 'You are already subscribed! Thank you.',]
-        
-        );
+    public function store (StoreSubscriberRequest $request) {
+        $data = $request->validated();
         Subscriber::create($data);
         return back()->with('status' , 'Subscribers Successful');
     }
