fc90db867c3e75df04a6a7fc4bda7b0dc779f12a
Abdullah Bamaga <abdallahbamaga999@gmail.com>
2026-02-23 23:14:13 +0300

    Implement create blog view: Add form with categories dropdown and file upload support

--- resources/views/theme/blogs/create.blade.php
+++ resources/views/theme/blogs/create.blade.php
@@ -1,10 +1,10 @@
 @extends('theme.master')
-@section('title', 'register')
+@section('title', 'add new blog')
 
 @section('content')
     @include('theme.partisal.hero', [
-        'title' => 'Register',
-        'title1' => 'register us',
+        'title' => 'Add New Blogs',
+        'title1' => 'New Blogs',
     ])
 
     <!-- ================ contact section start ================= -->
@@ -12,39 +12,42 @@
         <div class="container">
             <div class="row">
                 <div class="col-12">
-                    <form action="{{ route('register.post') }}" class="form-contact contact_form" method="post"
-                        novalidate="novalidate">
-                        <div class="row">
-                            @csrf
-                            <div class="col-6">
-                                <div class="form-group">
-                                    <input class="form-control border" name="name" type="text"
-                                        placeholder="Enter your name" value="{{ old('name') }}">
-                                        <x-input-error :messages="$errors->get('name')" class="mt-2" />
-                                </div>
-                                <div class="form-group">
-                                    <input class="form-control border" name="email" type="email"
-                                        placeholder="Enter email address" value="{{ old('email') }}">
-                                        <x-input-error :messages="$errors->get('email')" class="mt-2" />
-                                </div>
-                            </div>
-                            <div class="col-6">
-                                <div class="form-group">
-                                    <input class="form-control border" name="password" id="name" type="password"
-                                        placeholder="Enter your password">
-                                        <x-input-error :messages="$errors->get('password')" class="mt-2" />
-                                        
-                                </div>
-                                <div class="form-group">
-                                    <input class="form-control border" name="password_confirmation" type="password"
-                                        placeholder="Enter your password confirmation">
-                                        <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
-                                </div>
-                            </div>
+                    <form action="{{ route('blogs.store') }}" class="form-contact contact_form" method="post"
+                        novalidate="novalidate" enctype="multipart/form-data">
+                        @csrf
+                        <div class="form-group">
+                            <input class="form-control border" name="name" type="text" placeholder="Enter your name"
+                                value="{{ old('name') }}">
+                            <x-input-error :messages="$errors->get('name')" class="mt-2" />
+                        </div>
+                        <div class="form-group">
+                            <label for="customImage" class="form-control border"
+                                style="cursor: pointer; padding-top: 10px; color: #777;">
+                                📁 Choose an image...
+                            </label>
+
+                            <input id="customImage" name="image" type="file" style="display: none;">
+
+                            <x-input-error :messages="$errors->get('image')" class="mt-2" />
+                        </div>
+                        <div class="form-group">
+                            <select class="form-control border" name="category_id" placeholder="Enter email address"
+                                value="{{ old('category_id') }}">
+                                <option value="">Select Category</option>
+                                @if (count($categories) > 0)
+                                    @foreach ($categories as $category)
+                                        <option value="{{ $category->id }}">{{ $category->name }}</option>
+                                    @endforeach
+                                @endif
+                            </select>
+                            <x-input-error :messages="$errors->get('category_id')" class="mt-2" />
+                        </div>
+                        <div class="form-group">
+                            <textarea class="w-100 border" name="description" placeholder="Enter your name" rows="5">{{ old('description') }}</textarea>
+                            <x-input-error :messages="$errors->get('description')" class="mt-2" />
                         </div>
                         <div class="form-group text-center text-md-right mt-3">
-                            <a href="{{ route('login') }}" class="mx-3">Already Registered ?</a>
-                            <button type="submit" class="button button--active button-contactForm">Register</button>
+                            <button type="submit" class="button button--active button-contactForm">Submit</button>
                         </div>
                     </form>
                 </div>
