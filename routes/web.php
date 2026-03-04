946df4f3c0764396ef9524291188f6ad0ef9cac9
Abdullah Bamaga <abdallahbamaga999@gmail.com>
2026-03-04 05:56:29 +0300

    Create AdminController, protected routes, and admin dashboard view

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,9 @@ use App\Http\Controllers\ContactController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\SubscriberController;
 use App\Http\Controllers\ThemeController;
+use App\Http\Controllers\AdminController;
 use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -57,6 +59,10 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::middleware(['auth' , 'admin'])->prefix('admin')->name('admin.')->group(function () {
+    Route::get('/dashboard' , [AdminController::class , 'index'])->name('dashboard');
 });
 
 require __DIR__.'/auth.php';
